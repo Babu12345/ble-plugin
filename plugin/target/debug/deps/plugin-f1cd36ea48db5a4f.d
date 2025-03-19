9d8fe0c5fb6132a2546b302c699ba1e85cbeca2c
Babu-Abel Wanyeki <bwanyeki12@gmail.com>
2025-03-19 11:40:40 -0400

    Adding a host and plugin

---
+++ plugin/target/debug/deps/plugin-f1cd36ea48db5a4f.d
@@ -0,0 +1,5 @@
+/Users/babuwanyeki/Documents/Rusty/ble-plugin/plugin/target/debug/deps/libplugin-f1cd36ea48db5a4f.rmeta: src/main.rs
+
+/Users/babuwanyeki/Documents/Rusty/ble-plugin/plugin/target/debug/deps/plugin-f1cd36ea48db5a4f.d: src/main.rs
+
+src/main.rs:
