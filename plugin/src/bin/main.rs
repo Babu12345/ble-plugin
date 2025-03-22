f0ad2ae1a7da8acb97d9911570d3d7674d22e5b9
Babu-Abel Wanyeki <bwanyeki12@gmail.com>
2025-03-22 11:19:01 -0400

    Adding starter code for the plug-in bluetooth interface

--- plugin/src/bin/main.rs
+++ plugin/src/bin/main.rs
@@ -1,3 +1,13 @@
 fn main() {
+    // Can I re-use embassy_usb and NimBLE for usb device data transfer
     println!("Hello, world!");
+    // let device = BLEDevice::take();
+    // let ble_advertising = device.get_advertising();
+
+    // device
+    //     .security()
+    //     .set_auth(AuthReq::all())
+    //     .set_passkey(123456)
+    //     .set_io_cap(SecurityIOCap::DisplayOnly)
+    //     .resolve_rpa();
 }
--- plugin/target/debug/deps/plugin-f1cd36ea48db5a4f.d
+++
@@ -1,5 +0,0 @@
-/Users/babuwanyeki/Documents/Rusty/ble-plugin/plugin/target/debug/deps/libplugin-f1cd36ea48db5a4f.rmeta: src/main.rs
-
-/Users/babuwanyeki/Documents/Rusty/ble-plugin/plugin/target/debug/deps/plugin-f1cd36ea48db5a4f.d: src/main.rs
-
-src/main.rs:
