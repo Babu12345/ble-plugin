338acd661b56c0fc07b209a77daa74c390cf299b
Babu-Abel Wanyeki <bwanyeki12@gmail.com>
2025-03-23 19:33:46 -0400

    Adding BLE starter code

--- plugin/src/bin/main.rs
+++ plugin/src/bin/main.rs
@@ -1,13 +1,19 @@
+use esp32_nimble::{
+    enums::{AuthReq, SecurityIOCap},
+    BLEDevice,
+};
+
+// See https://github.com/taks/esp32-nimble/blob/main/examples/ble_secure_server.rs
 fn main() {
     // Can I re-use embassy_usb and NimBLE for usb device data transfer
     println!("Hello, world!");
-    // let device = BLEDevice::take();
-    // let ble_advertising = device.get_advertising();
+    let device = BLEDevice::take();
+    let _ble_advertising = device.get_advertising();
 
-    // device
-    //     .security()
-    //     .set_auth(AuthReq::all())
-    //     .set_passkey(123456)
-    //     .set_io_cap(SecurityIOCap::DisplayOnly)
-    //     .resolve_rpa();
+    device
+        .security()
+        .set_auth(AuthReq::all())
+        .set_passkey(123456)
+        .set_io_cap(SecurityIOCap::DisplayOnly)
+        .resolve_rpa();
 }
