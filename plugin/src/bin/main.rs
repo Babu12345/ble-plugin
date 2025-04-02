00ac44afb0d8017f388e5dbbeab9750ad097897a
Babu-Abel Wanyeki <bwanyeki12@gmail.com>
2025-04-01 20:31:38 -0400

    update driver code

--- plugin/src/bin/main.rs
+++ plugin/src/bin/main.rs
@@ -2,6 +2,7 @@ use esp32_nimble::{
     enums::{AuthReq, SecurityIOCap},
     BLEDevice,
 };
+use esp_idf_sys::{tinyusb_config_t, tinyusb_driver_install, ESP_OK};
 
 // See https://github.com/taks/esp32-nimble/blob/main/examples/ble_secure_server.rs
 // See https://github.com/espressif/esp-idf/blob/v5.4.1/examples/peripherals/usb/device/tusb_serial_device/main/tusb_serial_device_main.c
@@ -17,4 +18,17 @@ fn main() {
         .set_passkey(123456)
         .set_io_cap(SecurityIOCap::DisplayOnly)
         .resolve_rpa();
+
+    let tusb_config = tinyusb_config_t {
+        external_phy: false,
+        self_powered: false,
+        ..Default::default()
+    };
+
+    unsafe {
+        let res = tinyusb_driver_install(&tusb_config);
+        if res != ESP_OK {
+            log::error!("Error installing driver")
+        }
+    }
 }
