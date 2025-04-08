fa9a6c323929a64c63aa8eb2ef481220fcf2c6b4
Babu-Abel Wanyeki <bwanyeki12@gmail.com>
2025-04-07 23:18:01 -0400

    Add more functionality to errors

--- plugin-no-std/src/error.rs
+++ plugin-no-std/src/error.rs
@@ -1,8 +1,14 @@
 //! Contains errors for the crate
 
+use thiserror_no_std::Error;
+
 /// Crate errors
-#[derive(Debug)]
-pub enum PluginError {}
+#[derive(Debug, Error)]
+pub enum PluginError {
+    /// Custom error. Usually just for testing
+    #[error("Custom error with message {0}")]
+    Custom(&'static str),
+}
 
 /// Result type for the configured crate errors
 pub type PluginResult<T> = core::result::Result<T, PluginError>;
