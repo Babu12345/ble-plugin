e6ee3d5369ed1140b23d106d27c90f638206aa43
Babu-Abel Wanyeki <bwanyeki12@gmail.com>
2025-06-14 14:40:04 -0400

    Updated cherry usb

--- host-std/cherry_usb_host_bindings.h
+++ host-std/cherry_usb_host_bindings.h
@@ -1,2 +1,4 @@
 // https://cherryusb.readthedocs.io/zh-cn/latest/index.html#
+// https://github.com/cherry-embedded/CherryUSB/blob/master/class/cdc/usbh_cdc_ncm.h
 #include "usbh_core.h"
+#include "usbh_cdc_ncm.h"
