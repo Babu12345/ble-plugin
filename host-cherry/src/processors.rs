78b335892c3e0651850df111341d740903894678
Babu-Abel Wanyeki <bwanyeki12@gmail.com>
2025-06-19 10:05:08 -0400

    Update cherry usb
    1. Add optional type for the CDC_LOCKER to capture the state where the cdc driver type has been deinitialized
    2. Ensure tx and rx types are the smae

--- host-cherry/src/processors.rs
+++ host-cherry/src/processors.rs
@@ -1,6 +1,6 @@
 use std::{
     sync::{
-        OnceLock,
+        RwLock,
         mpsc::{Receiver, SyncSender},
     },
     time::Duration,
@@ -11,32 +11,55 @@ use esp_idf_sys::cherry_host::{
 };
 use heapless::String;
 
-static LOCKER: OnceLock<ThreadSafeWrapper> = OnceLock::new();
+static CDC_LOCKER: RwLock<Option<ThreadSafeCDCWrapper>> = RwLock::new(None);
 pub type T = String<256>;
 
 #[derive(Debug)]
-struct ThreadSafeWrapper(*mut usbh_cdc_acm);
-unsafe impl Send for ThreadSafeWrapper {}
-unsafe impl Sync for ThreadSafeWrapper {}
+struct ThreadSafeCDCWrapper(*mut usbh_cdc_acm);
+unsafe impl Send for ThreadSafeCDCWrapper {}
+unsafe impl Sync for ThreadSafeCDCWrapper {}
+
+/// Initialization
+/// https://github.com/zleihao/CherryUSB-CDC-MSC/blob/50095e0b63bbdf6f2d5597e71edfa45dd8be6c1d/cdc_msc/middlewares/CherryUSB-1.4.0/class/cdc/usbh_cdc_acm.c#L170
+/// https://github.com/cherry-embedded/CherryUSB/blob/f23f5494920b64987350abc87c8154f410c6f5f9/platform/nuttx/usbh_serial.c#L180
+/// https://github.com/search?q=repo%3Acherry-embedded%2FCherryUSB%20usbh_cdc_acm_run&type=code
+/// https://github.com/hpmicro/zephyr_sdk_glue/blob/2a17ddea9f43eac3b7f57a0058ce49023d5fd06f/samples/cherryusb/host/cdc_acm/src/cdc_acm_chost.c#L33
 
 /// Strong reference to the cdc runner defined in C
 #[unsafe(no_mangle)]
 pub extern "C" fn usbh_cdc_acm_run(cdc_acm_class: *mut usbh_cdc_acm) {
-    LOCKER.set(ThreadSafeWrapper(cdc_acm_class)).unwrap();
+    *CDC_LOCKER.write().unwrap() = Some(ThreadSafeCDCWrapper(cdc_acm_class));
+}
+
+/// Strong reference to the cdc acm stopper defined in C
+#[unsafe(no_mangle)]
+#[allow(unused_variables)]
+pub extern "C" fn usbh_cdc_acm_stop(cdc_acm_class: *mut usbh_cdc_acm) {
+    *CDC_LOCKER.write().unwrap() = None;
 }
 
 pub unsafe fn receive_usb_data(sender: SyncSender<T>) {
-    LOCKER.wait();
     loop {
-        let mut buffer = [0u8; 10];
+        let cdc_acm_class: *mut usbh_cdc_acm = match CDC_LOCKER.read().unwrap().as_ref() {
+            Some(wrapper) => wrapper,
+            None => continue,
+        }
+        .0;
+        let mut buffer: T = String::new();
         // TODO: Validate with return value
-        let _length = unsafe {
+        match unsafe {
             usbh_cdc_acm_bulk_in_transfer(
-                LOCKER.get().unwrap().0,
+                cdc_acm_class,
                 buffer.as_mut_ptr(),
                 buffer.len() as u32,
                 u32::MAX,
-            );
+            )
+        } {
+            x if x < 0 => {
+                log::error!("Unable to receive the data");
+                continue;
+            }
+            _ => {}
         };
         // TODO: Fix stack overflow here
         // let data: Vec<u8, 256> = Vec::from_slice(&buffer).unwrap();
@@ -47,8 +70,12 @@ pub unsafe fn receive_usb_data(sender: SyncSender<T>) {
 }
 
 pub unsafe fn send_usb_data(receiver: Receiver<T>) {
-    LOCKER.wait();
     loop {
+        let cdc_acm_class: *mut usbh_cdc_acm = match CDC_LOCKER.read().unwrap().as_ref() {
+            Some(wrapper) => wrapper,
+            None => continue,
+        }
+        .0;
         let mut data = match receiver.recv() {
             Ok(data) => data,
             Err(e) => {
@@ -56,15 +83,21 @@ pub unsafe fn send_usb_data(receiver: Receiver<T>) {
                 continue;
             }
         };
-        // TODO: Validate with return value
-        let _length = unsafe {
+        match unsafe {
             usbh_cdc_acm_bulk_out_transfer(
-                LOCKER.get().unwrap().0,
+                cdc_acm_class,
                 data.as_mut_ptr(),
                 data.len() as u32,
                 u32::MAX,
-            );
+            )
+        } {
+            x if x < 0 => {
+                log::error!("Unable to send the data {data}");
+                continue;
+            }
+            _ => {}
         };
+
         log::info!("Data transmitted: {data}");
     }
 }
