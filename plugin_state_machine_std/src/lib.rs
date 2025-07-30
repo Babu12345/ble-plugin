9a2ca076e71d9812941f191485df27874045aa62
Babu-Abel Wanyeki <bwanyeki12@gmail.com>
2025-07-30 18:57:36 -0400

    Start creating the state machine

--- plugin_state_machine_std/src/lib.rs
+++ plugin_state_machine_std/src/lib.rs
@@ -1,10 +1,12 @@
 #![deny(missing_docs)]
-// use protoco
 //! This library is used to to contain the complete processing logic and state machine to facilitate data/command transfer from BLE
 //! to usb and visa versa.
 
+use std::time::Duration;
+
 use esp32_nimble::BLEDevice;
 use protocol::DEFAULT_PACKET_SIZE;
+use protocol::io_types::{HostCommandConfigurePeripheral, HostCommandConfigureService};
 use protocol::plugin::plugin::{PluginReceiver, PluginSender};
 /// Contains state machine to process BLE and usb data and facilitate their data transfer
 pub struct PluginStateMachine {
@@ -13,6 +15,9 @@ pub struct PluginStateMachine {
     ble_device: BLEDevice,
 }
 
+/// There will be 2 runners the first will be processing
+/// usb data and sending it to BLE. The second will be processing
+/// BLE data and sending it to USB.
 impl PluginStateMachine {
     /// Create a new instance of the processing state machine
     pub fn new(
@@ -25,6 +30,38 @@ impl PluginStateMachine {
             usb_receiver,
             ble_device,
         }
+    }
+
+    /// This runner will process the USB data and send it to BLE.
+    pub fn usb_to_ble_runner(&mut self) {
+        loop {
+            match self.usb_receiver.receive() {
+                Ok(data) => {
+                    let maybe_cmd: Option<HostCommandConfigurePeripheral> = data.decode().ok();
+                    if let Some(cmd) = maybe_cmd {
+                        log::info!("Received USB command: {:?}", cmd);
+                    }
+
+                    let maybe_cmd: Option<HostCommandConfigureService> = data.decode().ok();
+                    if let Some(cmd) = maybe_cmd {
+                        log::info!("Received USB command: {:?}", cmd);
+                    }
+                }
+                Err(_) => {
+                    // Handle error, possibly log or retry
+                    log::error!("Failed to receive data from USB");
+                    // Add in a sleep so to not take up too much CPU time if there are repeated errors
+                    std::thread::sleep(Duration::from_millis(100));
+                    continue;
+                }
+            }
+        }
+    }
+
+    /// This runner will process the BLE data and send it to USB.
+    /// Processing should only happen once the BLE device has been setup and connected
+    pub fn ble_to_usb_runner(&mut self) {
+        loop {}
     }
 }
 
