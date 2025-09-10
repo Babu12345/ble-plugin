881be06411b262d409303c372a616adb288ff5df
Babu-Abel Wanyeki <bwanyeki12@gmail.com>
2025-09-10 19:31:34 -0400

    Create a started for plugin-std-nrf

---
+++ plugin-std-nrf/src/lib.rs
@@ -0,0 +1,5 @@
+#![deny(missing_docs)]
+// For now doesn't support the standard library
+#![no_std]
+//! NRF52 implementation of the BLE plugin
+// Docs: https://www.nordicsemi.com/Products/nRF52840
