8ce88712fba984cce64cc46c1e84463604aa8184
Babu-Abel Wanyeki <bwanyeki12@gmail.com>
2025-09-17 19:04:15 -0400

    Add reference to connections

--- device-embassy/src/processors.rs
+++ device-embassy/src/processors.rs
@@ -28,7 +28,7 @@ const BUFFER_SIZE: usize = 64;
 pub struct CdcAcmDeviceHost<'a, const CH_SIZE: usize, const BUFFER_SIZE: usize, M: RawMutex> {
     usb_device: UsbDevice<'a, Driver<'a>>,
     class: CdcAcmClass<'a, Driver<'a>>,
-    sender_connected: Option<Signal<M, bool>>,
+    sender_connected: Option<&'a Signal<M, bool>>,
     receiver_connected: Option<Signal<M, bool>>,
 }
 
@@ -87,7 +87,7 @@ impl<'a, const CH_SIZE: usize, const BUFFER_SIZE: usize, M: RawMutex>
     }
 
     /// Add a signal for sender connection
-    pub fn add_sender_connection_signal(mut self, signal: Signal<M, bool>) -> Self {
+    pub fn add_sender_connection_signal(mut self, signal: &'a Signal<M, bool>) -> Self {
         self.sender_connected = Some(signal);
         self
     }
