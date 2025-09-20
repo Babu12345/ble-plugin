a51f462aba4f24a18feb3b4b87511d9ee9937011
Babu-Abel Wanyeki <bwanyeki12@gmail.com>
2025-09-19 23:25:35 -0400

    Update the name in the profile configuration

--- plugin_state_machine_std/src/lib.rs
+++ plugin_state_machine_std/src/lib.rs
@@ -788,9 +788,6 @@ where
         match self.metadata.get_or_init_name(&mut self.ns).as_ref() {
             Some(name) => {
                 let mut adv_data_base = esp32_nimble::BLEAdvertisementData::new();
-                // Update the device name right before advertising to make sure that they're consistent
-                // TODO: Investigate what happens if the clearing of the services doesn't happen before this.
-                Self::set_device_name(name.as_str());
                 let adv_data = adv_data_base.name(name.as_str());
 
                 // Get all service UUIDs to include in advertisement
@@ -1366,6 +1363,13 @@ where
     }
 
     fn handle_configure_profile(&mut self, cmd: HostCommandConfigureProfile) -> Result<()> {
+        // Update the device name right before advertising to make sure that they're consistent
+        // TODO: Investigate what happens if the clearing of the services doesn't happen before this.
+        if let Some(name) = self.metadata.get_or_init_name(&mut self.ns) {
+            Self::set_device_name(name.as_str());
+            log::info!("Configured device name")
+        }
+
         log::info!("Configuring BLE profile: {:?}", cmd.profile);
 
         match BleProfile::try_from(cmd.profile) {
