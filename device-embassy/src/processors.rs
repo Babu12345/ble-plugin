c2179089dd59a01c5e71ba8c146b83b0fee3b2b9
Babu-Abel Wanyeki <bwanyeki12@gmail.com>
2025-10-26 20:37:31 -0400

    Update sizes

--- device-embassy/src/processors.rs
+++ device-embassy/src/processors.rs
@@ -39,9 +39,9 @@ impl<'a, const CH_SIZE: usize, const BUFFER_SIZE: usize, M: RawMutex>
     pub fn new(
         usb: Usb<'a>,
         ep_out_buffer: &'a mut [u8; 1024],
-        config_descriptor: &'a mut [u8; 256],
-        bos_descriptor: &'a mut [u8; 256],
-        control_buf: &'a mut [u8; 64],
+        config_descriptor: &'a mut [u8; 1024],
+        bos_descriptor: &'a mut [u8; 1024],
+        control_buf: &'a mut [u8; 1024],
         state: &'a mut State<'a>,
         is_self_powered: bool,
     ) -> Self {
