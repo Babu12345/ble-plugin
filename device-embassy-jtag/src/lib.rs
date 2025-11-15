885d4acabcced87ac99dea9817e68702ae957556
Babu-Abel Wanyeki <bwanyeki12@gmail.com>
2025-11-14 22:43:20 -0500

    Allow using the riscv32 architecture for the jtag integration

--- device-embassy-jtag/src/lib.rs
+++ device-embassy-jtag/src/lib.rs
@@ -2,7 +2,7 @@
 
 #![no_std]
 #![deny(missing_docs)]
-#![cfg(all(target_arch = "xtensa", target_os = "none"))]
+#![cfg(all(any(target_arch = "xtensa", target_arch = "riscv32"), target_os = "none"))]
 // Example: https://github.com/esp-rs/esp-hal/blob/main/examples/async/embassy_usb_serial_jtag/src/main.rs
 
 mod processors;
