f6fca281e1b54b4beca7f9fb99c334ff88f872f6
Babu-Abel Wanyeki <bwanyeki12@gmail.com>
2025-11-16 12:52:42 -0500

    Implement AsyncHostProcessor for DeviceHostUart

--- device-embassy-uart/src/processors.rs
+++ device-embassy-uart/src/processors.rs
@@ -10,7 +10,7 @@ use embassy_sync::{
 };
 use embassy_time::Timer;
 use embedded_io_async::{Read, Write};
-use esp_hal::usb_serial_jtag::UsbSerialJtag;
+use esp_hal::{uart::Uart, Async, DriverMode};
 use protocol::{
     devices::host::AsyncHostProcessor,
     host::{AsyncHostReceiver, AsyncHostSender},
@@ -20,19 +20,25 @@ use crate::errors::Result;
 
 const BUFFER_SIZE: usize = 64;
 
-/// Device host for jtag communication
-pub struct DeviceHostJtag<'d, const CH_SIZE: usize, const BUFFER_SIZE: usize, M: RawMutex> {
-    jtag: UsbSerialJtag<'d, esp_hal::Async>,
+/// Device host for UART communication
+pub struct DeviceHostUart<
+    'd,
+    Dm: DriverMode,
+    const CH_SIZE: usize,
+    const BUFFER_SIZE: usize,
+    M: RawMutex,
+> {
+    uart: Uart<'d, Dm>,
     receiver_connected: Option<&'d Signal<M, bool>>,
 }
 
-impl<'d, const CH_SIZE: usize, const BUFFER_SIZE: usize, M: RawMutex>
-    DeviceHostJtag<'d, CH_SIZE, BUFFER_SIZE, M>
+impl<'d, Dm: DriverMode, const CH_SIZE: usize, const BUFFER_SIZE: usize, M: RawMutex>
+    DeviceHostUart<'d, Dm, CH_SIZE, BUFFER_SIZE, M>
 {
     /// Create a new instance
-    pub fn new(jtag: UsbSerialJtag<'d, esp_hal::Async>) -> Self {
+    pub fn new(uart: Uart<'d, Dm>) -> Self {
         Self {
-            jtag,
+            uart,
             receiver_connected: None,
         }
     }
@@ -41,5 +47,79 @@ impl<'d, const CH_SIZE: usize, const BUFFER_SIZE: usize, M: RawMutex>
     pub fn add_connection_signal(mut self, signal: &'d Signal<M, bool>) -> Self {
         self.receiver_connected = Some(signal);
         self
+    }
+}
+
+impl<'d, const CH_SIZE: usize, M: RawMutex>
+    AsyncHostProcessor<CH_SIZE, BUFFER_SIZE, M, crate::errors::Error>
+    for DeviceHostUart<'d, Async, CH_SIZE, BUFFER_SIZE, M>
+{
+    type T<'ch>
+        = (
+        Sender<'ch, M, [u8; BUFFER_SIZE], CH_SIZE>,
+        Receiver<'ch, M, [u8; BUFFER_SIZE], CH_SIZE>,
+    )
+    where
+        M: 'ch;
+
+    fn processors<'ch>(
+        self,
+        to: Self::T<'ch>,
+        from: Self::T<'ch>,
+    ) -> Result<(
+        impl Future<Output = ()>,
+        AsyncHostSender<'ch, M, BUFFER_SIZE, CH_SIZE>,
+        AsyncHostReceiver<'ch, M, BUFFER_SIZE, CH_SIZE>,
+    )> {
+        let (mut rx, mut tx) = self.uart.split();
+
+        let processor_runner = async move {
+            let to_uart_fn = async {
+                Timer::after_millis(100).await;
+                log::info!("UART sender connection established");
+                loop {
+                    let data = to.1.receive().await;
+                    match tx.write_all(&data).await {
+                        Ok(_) => {
+                            let _ = tx.flush_async().await;
+                        }
+                        Err(_) => {
+                            log::warn!("UART write error, continuing");
+                        }
+                    }
+                }
+            };
+
+            let from_uart_fn = async {
+                Timer::after_millis(100).await;
+                log::info!("UART receiver connection established");
+                if let Some(signal) = &self.receiver_connected {
+                    signal.signal(true);
+                }
+                let mut buf = [0; BUFFER_SIZE];
+                loop {
+                    match rx.read(&mut buf).await {
+                        Ok(_len) => {
+                            from.0.send(buf).await;
+                            buf = [0; BUFFER_SIZE];
+                        }
+                        Err(_) => {
+                            log::warn!("UART read error, continuing");
+                            if let Some(signal) = &self.receiver_connected {
+                                signal.signal(false);
+                            }
+                        }
+                    }
+                }
+            };
+
+            join(to_uart_fn, from_uart_fn).await;
+        };
+
+        Ok((
+            processor_runner,
+            AsyncHostSender::new(to.0),
+            AsyncHostReceiver::new(from.1),
+        ))
     }
 }
--- host-uart-no-std/src/bin/main.rs
+++ host-uart-no-std/src/bin/main.rs
@@ -2,14 +2,9 @@
 #![no_main]
 
 use embassy_executor::Spawner;
-use embassy_sync::blocking_mutex::raw::NoopRawMutex;
-use embassy_sync::channel::Channel;
 use esp_backtrace as _;
-use esp_hal::usb_serial_jtag::UsbSerialJtag;
 use esp_hal::{clock::CpuClock, timer::systimer::SystemTimer};
 use host_uart_no_std::configs::initalize_logger;
-use protocol::DEFAULT_PACKET_SIZE;
-use protocol::devices::host::AsyncHostProcessor;
 
 // USB serial device example: https://github.com/esp-rs/esp-hal/blob/main/examples/async/embassy_usb_serial_jtag/src/main.rs
 #[esp_hal_embassy::main]
